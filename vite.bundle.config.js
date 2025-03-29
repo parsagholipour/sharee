871f92d77875768a4c4a2f10c7ed90e6fc027d05
Parsa <12parsa@gmail.com>
2025-03-29 12:00:51 +0330

    expose styles

--- vite.bundle.config.js
+++ vite.bundle.config.js
@@ -1,9 +1,43 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [cssInjectedByJsPlugin(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/styles/style.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-dropdown.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-fixed.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-hover.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-text.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-normal.css',
+          dest: 'style'
+        },
+        {
+          src: 'src/assets/styles/style-modal.css',
+          dest: 'style'
+        },
+      ]
+    })
+  ],
   build: {
     emptyOutDir: false,
     lib: {
